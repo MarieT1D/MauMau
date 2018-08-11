62048ad9397b925ee30795ab167b6607bdede245
Marie <37214302+MarieT1D@users.noreply.github.com>
2018-08-11 21:12:14 +0200

    Update maumau.py

--- maumau.py
+++ maumau.py
@@ -5,17 +5,17 @@ Kartendarstellung
 7       10      20      30      40
 8       11      21      31      41
 9       12      22      32      42
-koenig  13      23      33      43
-10      14      24      34      44
-sau     15      25      35      45
-unter   16      26      36      46
-ober    17      27      37      47
+10      13      23      33      43
+unter   14      24      34      44
+ober    15      25      35      45
+könig   16      26      36      46
+sau     17      27      37      47
 
 """
 import random
 #Mgliche Farben/Werte
 farbe=("Eichel", "Gras", "Herz", "Schelle")
-wert=("7","8","9","Koenig","10","Sau","Unter","Ober")
+wert=("7","8","9","10","Unter","Ober","König","Sau")
 
 #Darstellung im Bot
 pos_kartenwert_id=[i for i in range(8)]
@@ -43,17 +43,42 @@ def get_zeichen(karte):
     return pos_kartenzeichen[karte%10]
     
 
-class karte():
+class Karte():
     def __init__(self, id):
         
         self.id = id
         self.farbe   = get_farbe(id)
         self.zeichen = get_zeichen(id) 
-        self.wert    = get_punkte(id)
+        if self.zeichen== "7":
+                self.wert= "zwei_ziehen"
+        elif self.zeichen=="8":
+                self.wert= "aussetzen"
         self.name    = str(self.farbe)+'_'+str(self.zeichen)
         self.loc     = 'None'
     
-    #location encoding:
+class Game():
+        def __init__(self, players):
+                assert len(players) < 6
+                self.kartensatz=random.shuffle[Karte(i) for i in karten_ids]
+                self.players=players
+                for i in range(4):
+                        self.players[i].handout(self.kartensatz[(i*5): i*5+5])
+                        
+####Was bedeutet das (s. Schafkopf):
+        """    for j in self.kartensatz[i*8:(i+1)*8]:
+
+                j.move_loc(self.players[i].name)
+                """
+class Player(Game):
+        def __init__(name):
+                self.name=name
+                for i in range(players):
+                        self.id=yield(i)
+                
+                
+        
+        
+   #location encoding:
     #'None' - nicht ausgeteilt
     #str - name des Spielers
     #'Stich' - Im aktuellen Stich
